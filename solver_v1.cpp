f1ea62ecaabf62fbc92b5686d0d38e9b5f1b52e0
gasatig-oss <gasatig@gmail.com>
2025-09-17 11:44:19 +0200

    Update solver_v1.cpp
    Added some optimizations

--- solver_v1.cpp
+++ solver_v1.cpp
@@ -3,53 +3,49 @@
 class Solution {
 public:
 
-    bool isValid(vector<vector<char>>&board, int row, int col, char num)
+    bool isValid(vector<vector<char>>&board, int row, int col, int num)
     {
+        char c_num = num + '0';
         for(int i = 0; i < 9; i++)
         {
-            if (board[row][i] == num) return false;
-            if (board[i][col] == num) return false;
-            if (board[3*(row/3) + i/3][3*(col/3) + i%3] == num) return false;
+            if (board[row][i] == c_num) return false;
+            if (board[i][col] == c_num) return false;
+            if (board[3*(row/3) + i/3][3*(col/3) + i%3] == c_num) return false;
 
         }
         return true;
-
     }
-    bool solver_rec(vector<vector<char>>& board, int row, int col)
+
+    bool solver_rec(vector<vector<char>>& board)
     {
-        if (row == 9) return true;
-
-        int rowNext = row;
-        int colNext = col + 1;
-
-        if (colNext == 9)
+        for(int row = 0; row < 9; row++)
         {
-            rowNext = rowNext + 1;
-            colNext = 0;
-        }
-
-        if (board[row][col] != '.')
-        {
-            return solver_rec(board, rowNext, colNext);
-        }
-
-        for (char num = '1'; num <= '9'; num++)
-        {
-            if(isValid(board, row, col, num))
+            for(int col = 0; col < 9; col++)
             {
-                board[row][col] = num;
-                if(solver_rec(board, rowNext, colNext))
+                if(board[row][col] == '.')
                 {
-                    return true;
+                    for (int num = 1; num <= 9; num++)
+                    {
+                        if(isValid(board, row, col, num))
+                        {
+                            board[row][col] = num + '0';
+                            if(solver_rec(board))
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+                    return false;
                 }
-                board[row][col] = '.';
             }
         }
-
-        return false;
-
+        return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solver_rec(board, 0, 0);
+        solver_rec(board);
     }
 };
